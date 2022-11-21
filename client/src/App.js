fe85299e239bc062020d8d56c19d14bcfb63a3fa
Salvaarmanasco <salvador.armanasco@gmail.com>
2022-11-21 17:22:44 -0300

    agregando page 404

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,17 @@ import LandingPage from "./Components/LandingPage";
 import Home from "./Components/Home";
 import ActivityCreate from "./Components/ActivityCreate";
 import CountryDetail from "./Components/CountryDetail";
+import NotFound from "./Components/NotFound.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={LandingPage} />
-        <Route path="/home" component={Home} />
-        <Route path="/activities" component={ActivityCreate} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/activities" component={ActivityCreate} />
         <Route exact path="/countries/:id" component={CountryDetail} />
+        <Route path="*" component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
---
+++ client/src/Components/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../Styles/NotFound.module.css";
+
+function NotFound() {
+  return (
+    <div className={styles.notfound}>
+      <h1>404 Page Not Found</h1>
+    </div>
+  );
+}
+
+export default NotFound;
