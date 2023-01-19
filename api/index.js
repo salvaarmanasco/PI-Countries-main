97d0fb0854906b550dd9b80478f138cfdac7b040
Salvaarmanasco <salvador.armanasco@gmail.com>
2023-01-18 21:22:40 -0300

    PORT

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 require("dotenv").config();
 
-const PORT = process.env.DB_PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
